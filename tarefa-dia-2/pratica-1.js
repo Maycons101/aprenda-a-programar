f548f81101ddb53b7a7028d1425cbacb0f67f590
Maycons101 <mayconoliveira456@gmail.com>
2022-08-24 17:59:15 -0300

    informações

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,15 @@ let seuemail
 let suaprofissão 
 let suaidade
 let olugarquevocêsonhaumdiaconhcer
+
+seunome = "maycon de sousa oliveira"
+seuemail = "mayconoliveira456@gmail.com"
+suaprofissão = "estudante"
+suaidade = "20"
+olugarquevocêsonhaumdiaconhcer = "gramado"
+
+console.log (seunome)
+console.log (seuemail)
+console.log (suaprofissão)
+console.log (suaidade)
+console.log (olugarquevocêsonhaumdiaconhcer)
